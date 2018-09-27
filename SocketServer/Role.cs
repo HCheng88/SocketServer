934421fd224077e2a146e52835b436f72731a8a2
HCheng <1130004851@qq.com>
2018-09-27 11:23:29 +0800

    uu教程socker服务器

---
+++ SocketServer/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketServer
+{
+    /// <summary>
+    /// 角色，一个角色代表一个客户端
+    /// </summary>
+    public class Role
+    {
+        public ClicentSocket clicentSocket{ get; set; }
+    }
+}
---
+++ SocketServer/RoleMgr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketServer
+{
+   public class RoleMgr
+    {
+        //私有的构造防止在外部被实例化
+        private RoleMgr()
+        {
+            mAllRole = new List<Role>();
+        }
+        #region 单例
+        private static object lock_obg = new object();
+        private static RoleMgr Instance;
+
+        public static RoleMgr _instance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (lock_obg)
+                    {
+                        if (Instance == null)
+                            Instance = new RoleMgr();
+                    }
+                }
+                return Instance;
+            }
+        }
+        #endregion
+
+        private List<Role> mAllRole;
+        public List<Role> AllRole
+        {
+            get
+            {
+                return mAllRole;
+            }
+        }
+    }
+}
